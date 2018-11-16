8df0cfedf77c9759f58f5b1ec69210410551a39d
tpierce-dev <thecodegetter@gmail.com>
2018-11-15 22:25:27 -0500

    main.py added

---
+++ main.py
@@ -0,0 +1,18 @@
+import math
+
+while True:
+	menu = '''
+	Choose the math operation.
+	0 - Addition
+	1 - Subtraction
+	2 - Multiplication
+	3 - Division
+	4 - Modulo
+	5 - Raising to a Power
+	6 - Logarithm
+	7 - Sine
+	8 - Cosine
+	9 - Tangent
+	'''
+	print(menu)
+	oper = input("\nYour option from the menu: ")
